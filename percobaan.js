f9b788c7e8c91d77e1f0b0a6491edd22afec3097
ingga-dotcom <inggadewi.w@gmail.com>
2024-09-27 21:33:35 +0700

    percobaan.js

--- percobaan.js
+++ percobaan.js
@@ -6,8 +6,15 @@
 const http = require ('http')
 
 http.createServer ( function(request, response) {
-    response.writeHead(200,{'content-type': 'text/html'})    //kita akan mengirim text plain/mentah(semua yg ada di dlm kurung). Bisa juga pake text/html
-    response.end('<h1>WELCOME TO MY WEBSITE</h1><hr>')
+    response.writeHead(3000,{'content-type': 'text/html'})    //kita akan mengirim text plain/mentah(semua yg ada di dlm kurung). Bisa juga pake text/html
+    response.end(`<h1>WELCOME TO MY WEBSITE</h1><hr>
+        <button>klik</button>
+        <ul>
+            <li>ronaldo</li>
+            <li>messi</li>
+        </ul>`
+
+    )
 }).listen(3000,function(){
     console.log('server sudah nyala, buka http://localhost:3000')
 
