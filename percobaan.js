d569106d72edba2c766251289ee663ce845b7228
ingga-dotcom <inggadewi.w@gmail.com>
2024-09-30 19:52:01 +0700

    membuat halaman dan url

--- percobaan.js
+++ percobaan.js
@@ -6,17 +6,42 @@
 const http = require ('http')
 
 http.createServer ( function(request, response) {
-    response.writeHead(3000,{'content-type': 'text/html'})    //kita akan mengirim text plain/mentah(semua yg ada di dlm kurung). Bisa juga pake text/html
-    response.end(`<h1>WELCOME TO MY WEBSITE</h1><hr>
-        <button>klik</button>
-        <ul>
-            <li>ronaldo</li>
-            <li>messi</li>
+    console.log(request.url)
+    response.writeHead(200,{'content-type': 'text/html'})    //kita akan mengirim text plain/mentah(semua yg ada di dlm kurung). Bisa juga pake text/html
+
+    //halaman utama
+    if (request.url == '/') {
+    response.end (`<h1>WELCOME TO MY WEBSITE</h1><hr>`)
+    }
+    else if (request.url  == '/profile'){
+        response.end (
+        `<ul>
+            <li>nama lengkap : Dewi Ingga Wijayanti</li>
+            <li>alamat : central park residence</li>
+        </ul>`
+        )
+    }
+    else if (request.url == '/hubungi-saya') {
+        response.end (
+        `<ul>
+            <li>mobile : 601111001113</li>
+            <li>email : inggadewi.w@gmail.com</li>
         </ul>`
 
-    )
+        )   
+    }
+    
+
+
+    //untuk menangani url yg tdk ada
+    else {
+        response.end (`<h1> 404:halaman tidak ditemukan</h1><hr>`)
+    }
+
+
+
 }).listen(3000,function(){
-    console.log('server sudah nyala, buka http://localhost:3000')
+    console.log('server active, buka http://localhost:3000')
 
 })
 
