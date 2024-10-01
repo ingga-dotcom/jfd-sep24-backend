5b4548adfe40b859c9af6be8354bdd6111f7115c
ingga-dotcom <inggadewi.w@gmail.com>
2024-10-01 21:53:04 +0700

    view engine ejs & konsumsi data dari backend

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
+const { profile } = require('console')
 const express = require ('express')
 const app = express()
 
+app.set ('view engine' , 'ejs')  //setting penggunaan template engine untuk express
+app.set ('views' , './view-ejs')  //setting penggunaan folder untuk menyimpan file .ejs
+
+
+
+//function render ('nama-file')
+//nama file nya wajib ber eksensi .ejs
+//otomatis mengambil file .ejs yg ada di folder view.ejs
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World</h1>')
+  res.render('beranda')
 })
 
 app.get('/pendidikan', function (req, res) {  //bikin page /pendidikan
-    res.send('<h1>Riwayat Pendidikan</h1>')
+    let profil = {
+        nama: 'Prabowo',
+        s1: 'ITB : Sistem Informasi',
+        smk: 'SMK Remaja: Akuntansi',
+
+    }
+    res.render('page-pendidikan', profil)
 })
 
 app.get('/karyawan', function (req, res) { 
-    res.send('<h1>List Karyawans</h1>')
+    res.render('page-karyawan')
 })
 
 
