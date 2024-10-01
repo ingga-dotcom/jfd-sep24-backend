1d8cfa01b393aad80767ba3b665e8ccbd68b56f0
ingga-dotcom <inggadewi.w@gmail.com>
2024-10-01 14:14:42 +0700

    percobaan-mysql-get

---
+++ percobaan-mysql-get.js
@@ -0,0 +1,29 @@
+
+//buat variable , mysql adl nama var
+const mysql = require('mysql2')
+
+
+// sambungkan ke mysql
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'db_jfd_satu'
+})
+
+
+// buka koneksi
+db.connect( (error)=>{
+    if (error) {
+        throw error
+    } else {
+        console.log('berhasil tersambung ke mysql')
+    }
+})
+db.query('SELECT * FROM karyawan', function(errorSql, hasil) {
+    if (errorSql) {
+        console.log(errorSql)
+    } else {
+        console.log(hasil)
+    }
+})
