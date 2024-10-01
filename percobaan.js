5bb2eb0bdf33f8b6fa3c61da8489b7f107772119
ingga-dotcom <inggadewi.w@gmail.com>
2024-10-01 15:00:12 +0700

    file html terpisah

--- percobaan.js
+++ percobaan.js
@@ -1,37 +1,21 @@
 //memanggil modul bawaan dari node.js yaitu http
 //untuk membuat server http
-
-
-
 const http = require ('http')
 const fs = require ('fs')
 
 http.createServer ( function(request, response) {
-    console.log(request.url)
     response.writeHead(200,{'content-type': 'text/html'})    //kita akan mengirim text plain/mentah(semua yg ada di dlm kurung). Bisa juga pake text/html
+
 
     //halaman utama
     if (request.url == '/') {
         fs.createReadStream ('./view/halaman-utama.html').pipe(response)
     }
 
+
     //halaman profile
     else if (request.url  == '/profile'){
-
-        let tahun_lahir = 1945
-        let tahun_ini = 2024
-        let umur = tahun_ini - tahun_lahir
-
-        response.end (
-        `<ul>
-            <li>nama lengkap : Dewi Ingga Wijayanti</li>
-            <li>alamat : central park residence</li>
-            <li>tanggal lahir : 27 september ${tahun_lahir}</li>
-            <li>umur : ${umur} tahun</li>
-        </ul>
-        <br>
-        <a href="/">Balik ke beranda</a>`
-        )
+        fs.createReadStream('./view/halaman-profil.html').pipe(response)
     }
 
 
